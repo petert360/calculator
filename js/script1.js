8d8ff9e74b47170917cb97313bef0120b1b6b37f
petert360 <peter.t360@hotmail.com>
2020-11-28 23:12:41 +0100

    Működő számológép, de a kódot optimalizálni kell

--- js/script1.js
+++ js/script1.js
@@ -1,12 +1,6 @@
 //btnElement változót hozom létre, amihez hozzárendelem a kijelzőt.
 let displayElement = document.querySelector('.calc__display');
 
-// for loop segítségével minden gombon létrehozok egy eventlistenert, ami btnContent változóba tárolja a gomb tartalmát, és a tartalomtól függően funkciót renelek hozzá:
-// a "C" gomb rötli a kijelzőt
-// az egyenlőségjel meghívja a bevitt kifejezést végrahajtó függvényt.
-// minden egyéb gomb tartalma hozzáadódik egy akkumulátor változóhoz aminek a tartakme megjelenik a kijelzőn
-
-
 function parseExpression(str) {
     let numArr = str.split(/[-,+,x,÷]/).filter(Boolean); // regular expression segítségével kiveszem a számokat
     let strArr = str.split(/([-,+,x,÷])/).filter(Boolean); // regular expression segítségével tömbre alakítim a stringet
@@ -15,12 +9,9 @@ function parseExpression(str) {
             return true;
         }
     })
-    console.log(numArr);
-    console.log(opArr);
     return [numArr,
         opArr
     ];
-
     //let opArr = str.split(/[0,1,2,3,4,5,6,7,8,9,.]/).filter(Boolean); //ezzel nem sikerült pontosan kiemelni az operátorokat
 }
 
@@ -31,6 +22,37 @@ function checkArraysForError(arr) {
     return false;
 }
 
+function setDisplay(variable, display) {
+    displayVar = variable;
+    displayElement.textContent = display;
+}
+
+
+function calculate(parsedArrays) {  //parsedArrays[0] a számokat [1] az operátorokat tartalmazza
+    let nums = parsedArrays[0];
+    let ops = parsedArrays[1];
+    let result = Number(nums[0]);
+    for (let i = 1; i <= ops.length; i += 1) {
+        if (ops[i - 1] === '+') {
+            result = result + Number(nums[i]);
+        } else if (ops[i - 1] === '-') {
+            result = result - Number(nums[i]);
+        } else if (ops[i - 1] === 'x') {
+            result = result * Number(nums[i]);
+        } else if (ops[i - 1] === '÷') {
+            result = result / Number(nums[i]);
+        }
+    } return result;
+}
+
+
+
+
+// for loop segítségével minden gombon létrehozok egy eventlistenert, ami btnContent változóba tárolja a gomb tartalmát, és a tartalomtól függően funkciót renelek hozzá:
+// a "C" gomb törli a kijelzőt
+// az egyenlőségjel meghívja a bevitt kifejezést végrahajtó függvényt.
+// minden egyéb gomb tartalma hozzáadódik egy akkumulátor változóhoz aminek a tartalma megjelenik a kijelzőn
+
 let displayVar = '';
 let btnContent = '';
 let btnElements = document.querySelectorAll('.calc__btn');
@@ -38,14 +60,14 @@ for (let i = 0; i < btnElements.length; i += 1) {
     btnElements[i].addEventListener('click', function (event) {
         btnContent = event.target.textContent;
         if (btnContent === 'C') {
-            displayVar = '';
-            displayElement.textContent = '';
+            setDisplay('', '');
         } else if (btnContent === '=') {
             parsedArrays = parseExpression(displayVar);
-            // if (checkArraysForError(parsedArrays)) {
-            //     displayVar = 'Error'
-            // };
-            console.log(parsedArrays);
+            if (checkArraysForError(parsedArrays)) {
+                setDisplay('', 'Error');
+            } else {
+                setDisplay(calculate(parsedArrays), calculate(parsedArrays))
+            }
         }
         else {
             displayVar += event.target.textContent;
