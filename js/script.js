c898db5191238e050900fc0d2d7e61c7f44a6c75
petert360 <peter.t360@hotmail.com>
2020-11-29 00:19:23 +0100

    Kód javítás

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let displayElement = document.querySelector('.calc__display');
 
 function parseExpression(str) {
     let numArr = str.split(/[-,+,×,÷]/).filter(Boolean); // regular expression segítségével kiveszem a számokat
-    let strArr = str.split(/([-,+,×,÷])/).filter(Boolean); // regular expression segítségével tömbre alakítim a stringet
+    let strArr = str.split(/([-,+,×,÷])/).filter(Boolean); // regular expression segítségével tömbre alakítom a stringet
     let opArr = strArr.filter(function (e) {               // a teljes tömbböl kiszűröm az operátorokat
         if (e === '-' || e === '+' || e === '×' || e === '÷') {
             return true;
@@ -27,7 +27,7 @@ function setDisplay(variable, display) {
     displayElement.textContent = display;
 }
 
-function calculate(parsedArrays) {  //parsedArrays[0] a számokat [1] az operátorokat tartalmazza
+function calculate(parsedArrays) {  //parsedArrays[0] a számokat, az [1] az operátorokat tartalmazza
     let nums = parsedArrays[0];
     let ops = parsedArrays[1];
     let result = Number(nums[0]);
@@ -44,27 +44,20 @@ function calculate(parsedArrays) {  //parsedArrays[0] a számokat [1] az operát
     } return result;
 }
 
-// for loop segítségével minden gombon létrehozok egy eventlistenert, ami btnContent változóba tárolja a gomb tartalmát, és a tartalomtól függően funkciót renelek hozzá:
-// a "C" gomb törli a kijelzőt
-// az egyenlőségjel meghívja a bevitt kifejezést végrahajtó függvényt.
+// for loop segítségével minden btn__digit és btn__operator gombon létrehozok egy eventlistenert, ami a btnContent változóba tárolja a gomb tartalmát
 // minden egyéb gomb tartalma hozzáadódik egy akkumulátor változóhoz aminek a tartalma megjelenik a kijelzőn
-
 let displayVar = '';
 let btnContent = '';
 let btnElements = document.querySelectorAll('.btn__digit, .btn__operator');
 for (let i = 0; i < btnElements.length; i += 1) {
     btnElements[i].addEventListener('click', function (event) {
         btnContent = event.target.textContent;
-        if (btnContent === 'C') {
-            setDisplay('', '');
-        } 
-        else {
-            displayVar += event.target.textContent;
-            displayElement.textContent = displayVar;
-        }
+        displayVar += event.target.textContent;
+        displayElement.textContent = displayVar;
     })
 }
 
+// az egyenlőségjel meghívja hibaellenőrző illetve a bevitt kifejezést végrahajtó függvényt.
 document.querySelector('.btn__equation').addEventListener('click', () => {
     parsedArrays = parseExpression(displayVar);
     if (checkArraysForError(parsedArrays)) {
@@ -73,3 +66,6 @@ document.querySelector('.btn__equation').addEventListener('click', () => {
         setDisplay(calculate(parsedArrays), calculate(parsedArrays));
     }
 })
+
+// a "C" gomb törli a kijelzőt
+document.querySelector('.btn__clear').addEventListener('click', () => setDisplay('', ''))
